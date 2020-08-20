8dcd9b0898fd7e8197d359801838772c525a402e
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-20 14:02:11 -0400

    Recorrer arreglo de objetos

--- 01.js
+++ 01.js
@@ -2,6 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {
         titulo: 'Hola mundo con Vuejs',
-        frutas: ['Manzana', 'Pera', 'Platano']
+        frutas: [
+            {nombre: 'Pera', cantidad:10},
+            {nombre: 'Manzana', cantidad:0},
+            {nombre: 'Platano', cantidad:11}
+        ]
     }
 });
