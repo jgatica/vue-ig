7f9f40543e8678ad44474ee09374134e4b55c5bb
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-20 22:03:38 -0400

    Aumenta o disminuye barra. Color verde

--- 01.js
+++ 01.js
@@ -1,13 +1,17 @@
 const app = new Vue({
     el: '#app',
     data: {
-        mensaje: 'Mi mensaje'
+        mensaje: 'Mi mensaje',
+        porcentaje: 0
     },
     methods: {
     },
     computed: {
         invertido() {
             return this.mensaje.split('').reverse().join('');
+        },
+        manejarPorcentaje() {
+            return this.porcentaje;
         }
     }
 });
