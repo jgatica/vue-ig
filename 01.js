6b544389e3aeb0056241d25b7357fd9089c51091
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-20 21:52:20 -0400

    Se invierte cadena

--- 01.js
+++ 01.js
@@ -1,10 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
+        mensaje: 'Mi mensaje'
     },
     methods: {
     },
     computed: {
-
+        invertido() {
+            return this.mensaje.split('').reverse().join('');
+        }
     }
 });
