ebb510431a8b7fe4ccfa4e61b70c13b77fe99056
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-21 14:20:20 -0400

    Se ejecutan las funciones  beforeCreate, created, beforeMount y mounted. Y al pinchar boton Cambiar saludo, se ejecutan beforeUpdate y updated

--- 01.js
+++ 01.js
@@ -3,4 +3,36 @@ const app = new Vue({
     data: {
         saludo: 'Mi mensaje'
     },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        // Al crear los metodos, observadores y eventos, pero aun no accede al DOM
+        // Aun no se puede acceder a 'el'
+        console.log('created')
+    },
+    beforeMount() {
+        // Se ejecuta antes de insertar el DOM
+        console.log('beforeMount')
+    },
+    mounted() {
+        // Se ejecuta al insertar el DOM
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        // Al detectar un cambio
+        console.log('beforeUpdate')
+    },
+    updated() {
+        // Al realizar los cambios
+        console.log('updated')
+    },
+    beforeDestroy() {
+        // Antes de destruir la instancia
+        console.log('beforeDestroy')
+    },
+    destroyed() {
+        // Se destruye toda la instancia
+        console.log('destroyed')
+    },
 });
