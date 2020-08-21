982c96d2df0dd3d88ae545e519468f25ad55bf4a
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-21 14:08:27 -0400

    Seccion 3 - clase 18 Ciclo de vida -Base para empezar la clase

--- 01.js
+++ 01.js
@@ -1,21 +1,6 @@
 const app = new Vue({
     el: '#app',
     data: {
-        mensaje: 'Mi mensaje',
-        porcentaje: 0
+        saludo: 'Mi mensaje'
     },
-    methods: {
-    },
-    computed: {
-        invertido() {
-            return this.mensaje.split('').reverse().join('');
-        },
-        manejarPorcentaje() {
-            return {
-                'bg-success': this.porcentaje <= 10,
-                'bg-default': this.porcentaje > 10 && this.porcentaje <= 20,
-                'bg-danger': this.porcentaje > 20,
-            }
-        }
-    }
 });
