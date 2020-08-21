f1a4617aeb4ac59eb73e7a6d25b4e925a3c646fc
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-20 20:30:17 -0400

    16. Clases (v-bind:class) -Se agrega estructura base -Se agrega  CDN bootstrap

--- 01.js
+++ 01.js
@@ -1,35 +1,10 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'Hola mundo con Vuejs',
-        frutas: [
-            {nombre: 'Pera', cantidad:10},
-            {nombre: 'Manzana', cantidad:0},
-            {nombre: 'Platano', cantidad:11}
-        ],
-        nuevaFruta: '',
-        total: 0
     },
     methods: {
-        agregarFruta: function () {
-            this.frutas.push({
-                nombre: this.nuevaFruta,
-                cantidad: 0
-            })
-
-            this.nuevaFruta = ''
-        },
-        otroMetodo(){
-
-        }
     },
     computed: {
-        sumarFrutas() {
-            this.total = 0;
-            for (fruta of this.frutas) {
-                this.total += this.total + fruta.cantidad;
-            }
-            return this.total;
-        }
+
     }
 });
