d7ef2a6093dc85fcd8628d2526b364a534e9148a
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-21 17:06:21 -0400

    Se crea componente. Este sera llamado componente padre porque lo ocupare para luego agregar un componente hijo

---
+++ padre.js
@@ -0,0 +1,9 @@
+Vue.component('padre', {
+    template: `
+        <div class="bg-warning text-white p-2">
+            <div>
+                <h1>Este es el componente padre</h1>
+            </div>
+        </div>
+    `,
+});
